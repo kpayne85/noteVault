b11577278dbd192e2b23d3d7514f55c74f829cad
kpayne85 <notaek1@gmail.com>
2025-04-25 09:30:39 -0700

    simplified...

--- noteVault.js
+++ noteVault.js
@@ -1,37 +1,112 @@
-import color from './classes/color.js';
-import theme from './classes/theme.js';
-import button from './classes/button.js';
-import note from './classes/note.js';
-import category from './classes/category.js';
-import vault from './classes/vault.js';
+/**
+ * 
+ * @param {{
+ * type: String,
+ * text: String,
+ * id: String,
+ * classList: String,
+ * actions: [{event: String, func: function, target: HTMLElement}],
+ * appTarget: HTMLElement,
+ * appRelation: String,
+ * styles: [{prop: String, val: String}],}} propObj 
+ * @returns 
+ */
+function make({ type, text, id, classList, appTarget, appRelation, actions, styles }) {
 
+    //type, text, append, actions
+    if (type) {
+        const ele = document.createElement(type);
+        if (text) ele.innerText = text;
+        if (actions) {
+            actions.forEach((a) => {
+                ele.addEventListener(a.event, function (e) {
+                    if (e.target === ele && e.type === a.event) {
+                        a.func(e, a.target);
+                    }
+                });
+            })
+        }
+        if (id) ele.id = id;
+        if (classList) ele.classList = classList;
+        if (styles) {
+            styles.forEach((style) => {
+                ele.style[style.prop] = style.val;
+            });
+        }
 
-/************** End Classes ****************** */
-
-
-
-
-const green = new color("rgb(0,255,0)");
-const black = new color("rgb(0,0,0)");
-const darkGrey = new color("rgb(50,50,50)");
-const defaultTheme = new theme(black, darkGrey, black, green, green, new color(green.inverse()));
-const v = new vault(defaultTheme);
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    init();
-});
-
-//window.addEventListener('resize', v.setSize, true);
-
-
-function init() {
-    document.querySelector("body").appendChild(v.element);
-    v.setSize();
-
+        if (appTarget) {
+            switch (appRelation) {
+                case 'last child':
+                    appTarget.appendChild(ele);
+                    break;
+                default:
+            }
+        }
+        return ele;
+    }
 }
 
+function drawlayout() {
+    const background = make({
+        type: 'div',
+        id: 'background',
+        appTarget: document.querySelector('body'),
+        appRelation: 'last child',
+    });
+    const headerSection = make({
+        type: 'div',
+        id: 'header-section',
+        appTarget: background,
+        appRelation: 'last child',
+    });
+    const header = make({
+        type: 'h1',
+        id: 'header',
+        text: 'noteVault!',
+        appTarget: headerSection,
+        appRelation: 'last child',
+    });
+    const contentSection = make({
+        type: 'div',
+        id: 'content-section',
+        appTarget: background,
+        appRelation: 'last child',
+    });
+    const notesSection = make({
+        type: 'div',
+        id: 'notes-section',
+        appTarget: contentSection,
+        appRelation: 'last child',
+    });
+    const notesPreviewSection = make({
+        type: 'div',
+        id: 'notes-preview-section',
+        appTarget: contentSection,
+        appRelation: 'last child',
+    });
+}
 
+function drawNotesSection() {
+    const loc = document.querySelector("#notes-section");
+    const addCat = function (loc, relation) {
+        make({
+            type: 'ul',
+            text: "New Catagory",
+            classList: 'catagory',
+            appTarget: loc,
+            appRelation: relation,
+        });
+        console.log(loc,);
+    };
+    const newCatBtn = make({
+        type: 'button',
+        id: 'new-cat',
+        text: 'New Catagory',
+        appTarget: loc,
+        appRelation: 'last child',
+        actions: [{ event: 'click', func: function () { addCat(loc, 'last child'); }, target: loc }],
+    });
+}
 
-
-
+drawlayout();
+drawNotesSection();
